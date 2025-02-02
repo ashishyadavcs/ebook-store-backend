95064a8b3ab6487d92e57881ec9c814380a03541
Ashish Kumar <ashishbhu221306@gmail.com>
2025-02-03 04:35:44 +0530

    cookie domain added

--- src/utils/createcookie.js
+++ src/utils/createcookie.js
@@ -1,10 +1,15 @@
+import config from "../config/index.js";
+
+const domain = config.APP_URL.replace("https://", ".");
 export const createTokenCookies = async (req, res, next, tokens) => {
     res.cookie("accesstoken", tokens.accesstoken, {
         httpOnly: true,
+        domain: domain,
         expires: new Date(Date.now() + 60 * 60 * 1000), //5 seconds
     });
     res.cookie("refreshtoken", tokens.refreshtoken, {
         httpOnly: true,
+        domain: domain,
         expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), //  1 month
     });
 };
