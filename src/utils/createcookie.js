34082beb694feff09d62a997a17141755b1f1de9
Ashish Kumar <ashishbhu221306@gmail.com>
2025-02-03 04:46:36 +0530

    cookie secure

--- src/utils/createcookie.js
+++ src/utils/createcookie.js
@@ -5,11 +5,15 @@ export const createTokenCookies = async (req, res, next, tokens) => {
     res.cookie("accesstoken", tokens.accesstoken, {
         httpOnly: true,
         domain: domain,
+        secure: true,
+        sameSite: "lax",
         expires: new Date(Date.now() + 60 * 60 * 1000), //5 seconds
     });
     res.cookie("refreshtoken", tokens.refreshtoken, {
         httpOnly: true,
         domain: domain,
+        secure: true,
+        sameSite: "lax",
         expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), //  1 month
     });
 };
