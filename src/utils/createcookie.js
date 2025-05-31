2bd464f99c7bb194477edbdbdaaf6393b1b924e3
Ashish Kumar <ashishbhu221306@gmail.com>
2025-05-31 15:10:05 +0530

    cookie optimized

--- src/utils/createcookie.js
+++ src/utils/createcookie.js
@@ -1,24 +1,20 @@
-export const createTokenCookies = async (req, res, next, tokens, user) => {
-    res.cookie("accesstoken", tokens.accesstoken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        expires: new Date(Date.now() + 10 * 1000), //5 seconds
-    });
-    res.cookie("refreshtoken", tokens.refreshtoken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), //  1 month
-    });
-    res.cookie("_user", user._id.toString(), {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-    });
-    res.cookie("userrole", user.role, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-    });
+const isProd = process.env.NODE_ENV === "production";
+const shortLived = {
+    httpOnly: true,
+    secure: isProd,
+    sameSite: "strict",
+    expires: new Date(Date.now() + 10 * 1000),
+};
+const longLived = {
+    httpOnly: true,
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+};
+
+export const createTokenCookies = (req, res, next, tokens, user) => {
+    res.cookie("accesstoken", tokens.accesstoken, shortLived);
+    res.cookie("_user", user._id.toString(), shortLived);
+    res.cookie("userrole", user.role, shortLived);
+    res.cookie("refreshtoken", tokens.refreshtoken, longLived);
 };
