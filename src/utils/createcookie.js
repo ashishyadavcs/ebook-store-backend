3f3ec70202802027c85c004bae5c0b99421a78ae
Ashish Kumar <ashishbhu221306@gmail.com>
2025-07-20 12:27:36 +0530

    fix : cookie not being set in browser being block

--- src/utils/createcookie.js
+++ src/utils/createcookie.js
@@ -1,20 +1,22 @@
 const isProd = process.env.NODE_ENV === "production";
 const shortLived = {
     httpOnly: true,
-    secure: isProd,
-    sameSite: "strict",
-    expires: new Date(Date.now() + 10 * 1000),
+    secure: true,
+    sameSite: isProd ? "Strict" : "none",
+    expires: new Date(Date.now() + 10 * 60 * 1000), // 10 seconds
 };
 const longLived = {
     httpOnly: true,
-    secure: isProd,
-    sameSite: isProd ? "none" : "lax",
-    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    secure: true,
+    sameSite: isProd ? "Strict" : "none",
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
 };
 
-export const createTokenCookies = (req, res, next, tokens, user) => {
+export const createTokenCookies = (res, tokens, user) => {
     res.cookie("accesstoken", tokens.accesstoken, shortLived);
     res.cookie("_user", user._id.toString(), longLived);
     res.cookie("userrole", user.role, longLived);
     res.cookie("refreshtoken", tokens.refreshtoken, longLived);
 };
+
+//for samesite none secure should be true -> to be set in browser otherwise will be blocked
